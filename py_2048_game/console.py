8c4eff645a1200fba3de7dd14f01b8df2023befb
Anthony Monthe <anthony.monthe@gmail.com>
2021-12-21 03:50:47 +0000

    Added iterations

--- py_2048_game/console.py
+++ py_2048_game/console.py
@@ -11,6 +11,7 @@ logger = logging.getLogger('py2048_game')
 
 parser = argparse.ArgumentParser()
 parser.add_argument('action', default='solver', choices=('curses', 'solver'), nargs='?')
+parser.add_argument('--iterations', '-i', default=1, type=int)
 parser.add_argument('--solver', '-s', default='py_2048_game.solvers.RandomSolver')
 parser.add_argument('--verbose', '-v', default=3, type=int)
 parser.add_argument('--version', '-V', default=False, action="store_true")
@@ -30,15 +31,17 @@ def main():
         exit(0)
     elif args.action == 'curses':
         solver = solvers.get_solver(args.solver)()
-        play_curses.main(
-            solver=solver,
-        )
+        for i in range(args.iterations):
+            play_curses.main(
+                solver=solver,
+            )
     elif args.action == 'solver':
         solver = solvers.get_solver(args.solver)()
-        game = Game()
-        for _, action, reward in solver.solve_game(game):
-            logger.debug('Move: %s Score: %s', game.move_count, game.score)
-        logger.info('Score: %s', game.score)
+        for i in range(args.iterations):
+            game = Game()
+            for _, action, reward in solver.solve_game(game):
+                logger.debug('Move: %s Score: %s', game.move_count, game.score)
+            logger.info('Score: %s', game.score)
 
 if __name__ == "__main__":
     main()
