ada6387e3f784fa4c22cbc8054c44de07181cd8a
Anthony Monthe <anthony.monthe@gmail.com>
2021-12-22 00:06:06 +0000

    Added py_2048_game.solvers.FirstActionSolver

--- py_2048_game/solvers.py
+++ py_2048_game/solvers.py
@@ -4,12 +4,21 @@ from py_2048_game import utils
 
 
 class BaseSolver:
+    def pre_solve(self, game):
+        pass
+
+    def post_solve(self, game):
+        pass
+
     def solve(self, game):
         raise NotImplementedError("Method is missing.")
 
     def solve_game(self, game):
         while not game.game_over():
-            yield self.solve(game)
+            self.pre_solve(game)
+            output = self.solve(game)
+            self.post_solve(game)
+            yield output
 
 
 class RandomSolver(BaseSolver):
@@ -24,9 +33,30 @@ class RandomSolver(BaseSolver):
         )
 
 
+class FirstActionSolver(BaseSolver):
+    def __init__(self):
+        super().__init__()
+        self.action = 0
+
+    def pre_solve(self, game):
+        if not game.is_action_available(self.action):
+            self.action = 0 if self.action == 3 else (self.action + 1)
+
+    def solve(self, game):
+        reward = game.do_action(self.action)
+        return (
+            game.state,
+            self.action,
+            reward
+        )
+
+
+DEFAULT_SOLVER = RandomSolver
+
+
 def get_solver(path=None):
     if path is None:
-        return RandomSolver
+        return DEFAULT_SOLVER
     class_name = path.split('.')[-1]
     module_path = '.'.join([i for i in path.split('.')][:-1])
     solvers = importlib.import_module(module_path)
